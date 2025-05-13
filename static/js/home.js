a42b7d1a5e83bd13c37f8e9cccd286ca996e75d6
kit sitou <ctwohofa@gmail.com>
2025-05-12 17:22:27 -0700

    Update

--- static/js/home.js
+++ static/js/home.js
@@ -1,4 +1,28 @@
 function init(){
     console.log("home page")
 }
+
+
+let root = document.documentElement;
+
+root.addEventListener("mousemove", e => {
+  root.style.setProperty('--mouse-x', e.clientX + "deg");
+  root.style.setProperty('--mouse-y', e.clientY + "deg");
+});
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  const blocks = document.querySelectorAll('.block');
+  const io = new IntersectionObserver((entries, obs) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('visible');
+        obs.unobserve(entry.target);
+      }
+    });
+  }, { threshold: 0.1 });
+  
+  blocks.forEach(el => io.observe(el));
+});
+
 window.onload=init;
